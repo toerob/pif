c91ce2f79b02cd6a09e487edd75fa73561297753
Tomas Öberg <tomaserikoberg@gmail.com>
2023-05-09 23:48:09 +0200

    Add TODO

--- src/main.rs
+++ src/main.rs
@@ -14,9 +14,12 @@ use clap::Parser;
 use install::install_extensions;
 use list::list_extensions;
 
+
+// TODO: An update command that pulls from a git repo on "rift update" and updates extensions.json with new entries, 
+// TODO: stores it locally, in a workplace folder or close by within a .folder?
+
 fn main() -> () {
     let choice = InteractiveFictionToolArgs::parse();
-    
     match choice.menu {
         MenuSubCommand::List(cmd_args) => list_extensions(&cmd_args.list_options, &choice.global_options),
         MenuSubCommand::Install(cmd_args) => install_extensions(&cmd_args.name, &choice.global_options),
