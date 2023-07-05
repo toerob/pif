324bd43347d4679c1d9ec6df29df708b1c3f3aa1
Tomas Öberg <tomaserikoberg@gmail.com>
2023-07-05 13:11:30 +0200

    Add README and TODO markfiles

--- src/detect.rs
+++ src/detect.rs
@@ -23,24 +23,24 @@ pub fn detect_system() -> (InteractiveFictionSystem, Option<DirEntry>) {
     for entry in glob("*.{t3m}").expect("Failed to read tads3 glob pattern") {
         match entry {
             Ok(path) => return (InteractiveFictionSystem::Tads3, Some(path)),
-            Err(e) => {
-                println!("***ERROR**");
+            Err(_) => {
+                eprintln!("***ERROR**");
             }
         }
     }
     for entry in glob("*.{inf}").expect("Failed to read inform6 glob pattern") {
         match entry {
             Ok(_) => return (InteractiveFictionSystem::Inform6, None),
-            Err(e) => {
-                println!("***ERROR**");
+            Err(_) => {
+                eprintln!("***ERROR**");
             }
         }
     }
     for entry in glob("*.{dg}").expect("Failed to read dialog glob pattern") {
         match entry {
             Ok(_) => return (InteractiveFictionSystem::Dialog, None),
-            Err(e) => {
-                println!("***ERROR**");
+            Err(_) => {
+                eprintln!("***ERROR**");
             }
         }
     }
--- src/main.rs
+++ src/main.rs
@@ -1,106 +1,59 @@
 extern crate ansi_term;
 extern crate clap;
-extern crate serde;
+extern crate dirs;
+extern crate dotenv;
 extern crate git2;
-extern crate sublime_fuzzy;
 extern crate glob;
 extern crate globwalk;
-extern crate dotenv;
 extern crate home;
-extern crate dirs;
-extern crate semver;
-extern crate online;
 extern crate mockall;
+extern crate online;
+extern crate semver;
+extern crate serde;
+extern crate sublime_fuzzy;
 
-mod detect;
 mod args;
-mod model;
-mod update;
+mod color;
+mod common;
+mod detect;
 mod info;
 mod install;
 mod list;
 mod makefile;
-mod color;
-mod common;
+mod model;
+mod update;
 
-
-use std::{fs::{self}, process::exit};
+use std::fs::{self};
 
 use args::{InteractiveFictionToolArgs, MenuSubCommand};
-use clap::{Parser};
+use clap::Parser;
 use info::extensions_info;
 use install::install_extensions;
 use list::list_extensions;
-use semver::VersionReq;
 use update::update_extensions;
 
-use mockall::*;
-use mockall::predicate::*;
-
-
-
-// TODO: an info option that scans the folder and displays installed extensions along with their descriptions?
-// TODO: installation recipe that creates .ifp folder in the home directory. Clone settings to this
 fn main() -> () {
-
     let home_dir = dirs::home_dir().expect("Could not determine home directory. ");
-    //let ifp_home_dir = home_dir.join(".ifp");
-    let ifp_settings_dir = home_dir.join(".ifp/settings");
-    fs::create_dir_all(&ifp_settings_dir).expect("Could not create home and settings directory for ifp.");
-    let workspace_folder = ifp_settings_dir.as_os_str().to_str().expect("Conversion from PathBuf to str failed");
 
+    let ifp_settings_dir = home_dir.join(".ifp/settings");
+
+    fs::create_dir_all(&ifp_settings_dir)
+        .expect("Could not create home and settings directory for ifp.");
+
+    let workspace_folder = ifp_settings_dir
+        .as_os_str()
+        .to_str()
+        .expect("Conversion from PathBuf to str failed");
 
     let choice = InteractiveFictionToolArgs::parse();
     match choice.menu {
         MenuSubCommand::Update(_) => update_extensions(&choice.global_options, &workspace_folder),
         MenuSubCommand::Info(cmd_args) => extensions_info(&cmd_args.name, &choice.global_options),
-        MenuSubCommand::List(cmd_args) => list_extensions(&cmd_args.list_options, &choice.global_options),
-        MenuSubCommand::Install(cmd_args) => install_extensions(&cmd_args.name, &choice.global_options),
+        MenuSubCommand::List(cmd_args) => {
+            list_extensions(&cmd_args.list_options, &choice.global_options)
+        }
+        MenuSubCommand::Install(cmd_args) => {
+            install_extensions(&cmd_args.name, &choice.global_options)
+        }
     }
 }
-
-
-
-
-#[automock]
-trait Hello {
-    fn kalle(&self) -> u32  {
-        return 34;
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn mockall() {
-        println!("###");
-        let mut mockHello = MockHello::new();
-        
-        mockHello.expect_kalle()
-            .returning(|| 112);
-
-        let x = mockHello.kalle();
-        println!("**{}", &x.to_string());
-        assert_eq!(x, 112);
-
-
-    }
-}
-
-
-/*
-
-
-
-    //dotenv().expect("DotEnv could not load");
-    //let test_env = std::env::var("HOME").expect("HOME environment variable must be set.");
-
-
-    let mut cmd = Command::new("repl")
-        .version("1.0.0")
-        .propagate_version(true)
-        .multicall(true)
-        .subcommand(Command::new("foo").subcommand(Command::new("bar").arg(Arg::new("value"))));
-    cmd.build();*/
